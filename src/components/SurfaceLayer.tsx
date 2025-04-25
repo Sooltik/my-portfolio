7a74c348328ffa87cd973a05a382a858cc14ab10
Sultan Niazov <niazov.sultan@gmail.com>
2025-04-24 20:45:57 -0400

    improve palm tree

--- src/components/SurfaceLayer.tsx
+++ src/components/SurfaceLayer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Island from './Island';
 import WaterCreatures from './WaterCreatures';
 import WaveDivider from './WaveDivider';
 import '../styles/SurfaceLayer.css';
@@ -17,12 +16,57 @@ const SurfaceLayer: React.FC = () => {
       </div>
       
       <div className="layer-content">
-        <Island 
-          profileImage="img/intro-pic.jpg"
-          name="Sultan Niazov"
-          title="Full Stack Developer"
-          description="Dive deeper to explore my skills and projects"
-        />
+        <div className="island-container">
+          {/* Island with palm trees */}
+          <div className="island">
+            <div className="island-top">
+              {/* Profile elements moved inside the island-top */}
+              <div className="profile-container">
+                <div className="profile-outline"></div>
+                <img src="img/intro-pic.jpg" alt="Sultan Niazov" className="profile-pic" />
+                <div className="profile-shine"></div>
+              </div>
+              
+              <div className="intro-content">
+                <h1 className="intro-title">
+                  Hello, I'm <strong>Sultan Niazov</strong>
+                </h1>
+                <p className="intro-subtitle">Full Stack Developer</p>
+                <p className="intro-description">Dive deeper to explore my skills and projects</p>
+                <a href="#shallow" className="btn btn-dive">
+                  <span>Dive Down</span>
+                  <i className="fas fa-chevron-down"></i>
+                </a>
+              </div>
+            </div>
+            
+            {/* Palm tree left */}
+            <div className="palm-tree left">
+              <div className="palm-trunk"></div>
+              <div className="palm-leaves">
+                <div className="palm-leaf"></div>
+                <div className="palm-leaf"></div>
+                <div className="palm-leaf"></div>
+                <div className="palm-leaf"></div>
+                <div className="palm-leaf"></div>
+                <div className="palm-leaf"></div>
+              </div>
+            </div>
+            
+            {/* Palm tree right */}
+            <div className="palm-tree right">
+              <div className="palm-trunk"></div>
+              <div className="palm-leaves">
+                <div className="palm-leaf"></div>
+                <div className="palm-leaf"></div>
+                <div className="palm-leaf"></div>
+                <div className="palm-leaf"></div>
+                <div className="palm-leaf"></div>
+                <div className="palm-leaf"></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       
       <WaterCreatures />
